044882a860fffe2d70bd9592309d8962a9c2a258
ArchLeaders <archparliament@gmail.com>
2022-04-13 01:29:03 -0700

    Add Stream operations

---
+++ System.Operations/StreamOps.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Operations
+{
+    public static class StreamOps
+    {
+        public static byte[] ToBytes(this Stream stream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
