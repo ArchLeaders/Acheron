15fd95aacb1aa56233ec7dd952c3ef5f03455645
ArchLeaders <archparliament@gmail.com>
2022-04-13 01:28:41 -0700

    Remove ridiculous overloads; add hidden property

--- System.Operations/Execute.cs
+++ System.Operations/Execute.cs
@@ -21,8 +21,6 @@ namespace System.Operations
         /// <returns></returns>
         public static async Task Explorer(string directory) => await App("explorer.exe", directory);
 
-        #region App | Async/Void Functions
-
         /// <summary>
         /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
         /// </summary>
@@ -32,7 +30,7 @@ namespace System.Operations
         /// <param name="shellExecute">Opens the file with Windows Shell</param>
         /// <param name="workingDirectory">Directory in whcih the process starts in</param>
         /// <param name="output">Process output stream</param>
-        public static void App(string filename, string args, bool awaitExit, bool shellExecute, string workingDirectory, ref StreamReader output)
+        public static void App(string filename, string args, bool hidden = true, bool awaitExit, bool shellExecute, string workingDirectory, ref StreamReader output)
         {
             Process process = new();
 
@@ -40,6 +38,8 @@ namespace System.Operations
             process.StartInfo.Arguments = args;
             process.StartInfo.UseShellExecute = shellExecute;
             process.StartInfo.WorkingDirectory = workingDirectory;
+            process.StartInfo.CreateNoWindow = hidden;
+            process.StartInfo.WindowStyle = hidden ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal;
 
             if (output != null)
             {
@@ -63,7 +63,7 @@ namespace System.Operations
         /// <param name="awaitExit">Waits for the program to exit before proceeding.</param>
         /// <param name="shellExecute">Opens the file with Windows Shell</param>
         /// <param name="workingDirectory">Directory in whcih the process starts in</param>
-        public static async Task App(string filename, string args = "", bool awaitExit = true, bool shellExecute = true, string workingDirectory = ".\\")
+        public static async Task App(string filename, string args = "", bool hidden = true, bool awaitExit = true, bool shellExecute = true, string workingDirectory = ".\\")
         {
             Process process = new();
 
@@ -71,83 +71,13 @@ namespace System.Operations
             process.StartInfo.Arguments = args;
             process.StartInfo.UseShellExecute = shellExecute;
             process.StartInfo.WorkingDirectory = workingDirectory;
+            process.StartInfo.CreateNoWindow = hidden;
+            process.StartInfo.WindowStyle = hidden ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal;
 
             process.Start();
 
             if (awaitExit)
-            {
                 await process.WaitForExitAsync();
-            }
         }
-
-        #endregion
-
-        #region App | Overloads
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        /// <param name="awaitExit">Waits for the program to exit before proceeding.</param>
-        /// <param name="shellExecute">Opens the file with Windows Shell</param>
-        public static async Task App(string filename, string args, bool awaitExit, bool shellExecute) => await App(filename, args, awaitExit, shellExecute, ".\\");
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        /// <param name="awaitExit">Waits for the program to exit before proceeding.</param>
-        public static async Task App(string filename, string args, bool awaitExit) => await App(filename, args, awaitExit, true, ".\\");
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        public static async Task App(string filename, string args) => await App(filename, args, true, true, ".\\");
-
-        /// <summary>
-        /// Opens <paramref name="filename"/>.
-        /// </summary>
-        /// <param name="filename"></param>
-        public static async Task App(string filename) => await App(filename, "", true, true, ".\\");
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        /// <param name="awaitExit">Waits for the program to exit before proceeding.</param>
-        /// <param name="shellExecute">Opens the file with Windows Shell</param>
-        /// <param name="workingDirectory">Directory in whcih the process starts in</param>
-        public static async Task App(string filename, bool awaitExit, bool shellExecute, string workingDirectory, params string[] args) => await App(filename, string.Join(' ', args), awaitExit, shellExecute, workingDirectory);
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        /// <param name="awaitExit">Waits for the program to exit before proceeding.</param>
-        /// <param name="shellExecute">Opens the file with Windows Shell</param>
-        public static async Task App(string filename, bool awaitExit, bool shellExecute, params string[] args) => await App(filename, string.Join(' ', args), awaitExit, shellExecute, ".\\");
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        /// <param name="awaitExit">Waits for the program to exit before proceeding.</param>
-        public static async Task App(string filename, bool awaitExit, params string[] args) => await App(filename, string.Join(' ', args), awaitExit, true, ".\\");
-
-        /// <summary>
-        /// Opens <paramref name="filename"/> and passes <paramref name="args"/> to the newly created process.
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="args"></param>
-        public static async Task App(string filename, params string[] args) => await App(filename, string.Join(' ', args), true, true, ".\\");
-
-        #endregion
     }
 }
